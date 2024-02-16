ec96a26752e2b30e0cad320460efbd2b61617158
Muhammad Ali Momin <gibran5145@gmail.com>
2024-02-16 18:09:28 +0500

    Camel Race Done

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -9,4 +9,8 @@ public class StartMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
